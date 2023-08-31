140b1dce1bb5d657a68c44519113f6c8727bdc47
ruoduan-hub <chad97@126.com>
2023-08-31 16:22:45 +0800

    feat: response_format

--- api/create_image.ts
+++ api/create_image.ts
@@ -1,15 +1,18 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import OpenAI from "openai";
 
+
+type formatType = "url" | "b64_json" | null | undefined
+
 export default async function (
   request: VercelRequest,
   response: VercelResponse
 ) {
-  const { content = "Empty 404", n = 1, size = '512x512'} = request.query;
+  const { content = "Empty 404", n = 1, size = '512x512', response_format = 'url'} = request.query;
   const openai = new OpenAI({
     organization: process.env.OPENAI_Organization,
     apiKey: process.env.OPENAI_ApiKey,
   });
-  const image = await openai.images.generate({ prompt: String(content) });
+  const image = await openai.images.generate({ prompt: String(content), response_format: response_format as formatType });
   response.send(image.data);
 }
